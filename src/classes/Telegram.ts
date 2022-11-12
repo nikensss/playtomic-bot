7bcd747046db07802de6435857e5619e18cb62c3
Ricard Mallafre <rikitzzz@gmail.com>
2022-11-12 17:50:41 +0100

    feat: log errors, check chat id, ping command

--- src/classes/Telegram.ts
+++ src/classes/Telegram.ts
@@ -19,20 +19,44 @@ export class Telegram {
 
   init(): this {
     this.bot.onText(/\/check/, async msg => {
-      logger.info({ msg });
-      await this.bot.sendMessage(msg.chat.id, 'Let me check for you, just a moment...');
-      const promises = (await this.onCheck()).map(s => this.bot.sendMessage(msg.chat.id, s));
-      await Promise.all(promises);
+      try {
+        logger.info({ msg });
+        await this.bot.sendMessage(msg.chat.id, 'Let me check for you, just a moment...');
+        const messages = (await this.onCheck()).map(s => this.bot.sendMessage(msg.chat.id, s));
+        await Promise.all(messages);
+      } catch (err) {
+        logger.error({ err });
+        await this.sendError(err, msg);
+      }
     });
 
-    if (process.env.TELEGRAM_CHAT_ID) {
-      this.bot.sendMessage(process.env.TELEGRAM_CHAT_ID, "I'm listening...").catch(err => logger.error({ err }));
-    }
+    this.bot.onText(/\/ping/, msg => this.sendMessage(msg.chat.id, 'pong'));
+
+    this.bot.onText(/\/id/, msg => this.sendMessage(msg.chat.id, `${msg.chat.id}`));
+
+    this.sendAdminMessage("I'm listening...").catch(err => logger.error({ err }));
 
     return this;
   }
 
-  async onCheck(): Promise<string[]> {
+  private async sendError(err: unknown, msg: TelegramBot.Message): Promise<void> {
+    await this.sendAdminMessage(`Error in chat with "${msg.chat.username}": ${err}`);
+  }
+
+  private async sendAdminMessage(message: string): Promise<void> {
+    if (!process.env.TELEGRAM_CHAT_ID) return;
+    await this.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+  }
+
+  async sendMessage(chatId: TelegramBot.ChatId, message: string): Promise<void> {
+    try {
+      await this.bot.sendMessage(chatId, message);
+    } catch (err) {
+      logger.error({ err });
+    }
+  }
+
+  private async onCheck(): Promise<string[]> {
     const playtomic = new Playtomic(process.env.EMAIL, process.env.PASSWORD);
     const relevantTenants: Tenant[] = await playtomic.getRelevantTenants();
     for (const tenant of relevantTenants) {
