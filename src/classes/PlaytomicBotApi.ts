2936735c6ceb71d88f569ca13cfaf356e03602a1
Ricard Mallafre <rikitzzz@gmail.com>
2022-11-26 23:39:15 +0100

    feat: get clubs on telegram command

--- src/classes/PlaytomicBotApi.ts
+++ src/classes/PlaytomicBotApi.ts
@@ -21,17 +21,34 @@ type Courts = PlaytomicBotApiAvailabilityResponse[number]['courts'];
 type Availability = Courts[number]['availability'];
 type Slots = Availability[number]['slots'];
 
+type PlaytomicBotApiClubsResponse = {
+  tenant_id: string;
+  tenant_name: string;
+  address: {
+    street: string;
+    postal_code: string;
+    city: string;
+    country: string;
+  };
+}[];
+
 export class PlaytomicBotApi {
   private url: string;
   private secret: string;
+  private authorization: string;
 
-  constructor(url: string, secret: string) {
+  constructor(user: User) {
+    const [url, secret] = [process.env.PLAYTOMIC_BOT_API, process.env.JWT_SECRET];
+    if (!url) throw new Error('Missing Playtomic Bot API URL');
+    if (!secret) throw new Error('Missing JWT secret');
+
     this.url = url;
     this.secret = secret;
+    this.authorization = `Bearer ${jwt.sign(user, this.secret, { algorithm: 'HS512' })}`;
   }
 
-  async availability(user: User): Promise<string[]> {
-    const authorization = `Bearer ${jwt.sign(user, this.secret, { algorithm: 'HS512' })}`;
+  async availability(): Promise<string[]> {
+    const authorization = this.authorization;
     const response = await request(`${this.url}/playtomic/availability`, { headers: { authorization } });
     const availability = (await response.body.json()) as PlaytomicBotApiAvailabilityResponse;
 
@@ -39,6 +56,18 @@ export class PlaytomicBotApi {
       const stringifiedCourts = courtsToString(courts);
       if (stringifiedCourts) return `${name}:\n${courtsToString(courts) || '💩'}`;
       return `${name}: 💩`;
+    });
+  }
+
+  async findClub(name: string): Promise<{ title: string; id: string }[]> {
+    const authorization = this.authorization;
+    const response = await request(`${this.url}/playtomic/clubs`, { query: { name }, headers: { authorization } });
+
+    const clubs: PlaytomicBotApiClubsResponse = await response.body.json();
+
+    return clubs.map(({ tenant_id, tenant_name, address }) => {
+      const fullAddress = `${address.street}, ${address.postal_code}, ${address.city}, ${address.country}`;
+      return { title: `${tenant_name.trim()}: ${fullAddress}`, id: tenant_id };
     });
   }
 }
